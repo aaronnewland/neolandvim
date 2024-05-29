a784a4dfa43a28f99262caaa5b83e10d5a852a50
Aaron Newland <anewland7@gmail.com>
2024-05-28 22:06:36 -0400

    Bug fixes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,9 +1,11 @@
 local map = vim.keymap
 
 map.set("n", "<leader>ov", vim.cmd.Ex)
-map.set("n", "<leader>bn", ":enew<CR>", { desc = "create [b]uffer [n]ew" })
+map.set("n", "<leader>bn", "<cmd>enew<CR>", { desc = "create [b]uffer [n]ew" })
 map.set("n", "<leader>noh", "<cmd> set hlsearch! <CR>", { desc = "[n][o] [h]ighlight search toggle" })
 map.set("n", "<C-a>", "ggVG", { desc = "select [a]ll text in file" })
+
+map.set("i", "<C-c>", "<Esc>")
 
 -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
 -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
@@ -24,15 +26,15 @@ map.set("n", "<C-j>", "<C-w>j", { desc = "Window down" })
 map.set("n", "<C-k>", "<C-w>k", { desc = "Window up" })
 
 -------------------------------------- splitting windows ---------------------------------
-map.set("n", "<leader>|", ":vsp<CR>", { desc = "split window vertically [|]" })
-map.set("n", "<leader>-", ":sp<CR>", { desc = "split window horizontally [-]" })
+map.set("n", "<leader>|", "<cmd>vsp<CR>", { desc = "split window vertically [|]" })
+map.set("n", "<leader>-", "<cmd>sp<CR>", { desc = "split window horizontally [-]" })
 
 -------------------------------------- save ----------------------------------------------
-map.set("n", "<C-s>", ":w<CR>", { desc = "[s]ave file" })
+map.set("n", "<C-s>", "<cmd>w<CR>", { desc = "[s]ave file" })
 
 -------------------------------------- nvimtree ------------------------------------------
-map.set("n", "<C-n>", ":NvimTreeToggle<CR>", { desc = "toggle [n]vimtree" })
-map.set("n", "<leader>e", ":NvimTreeFocus<CR>", { desc = "focus nvimtr[e]e" })
+map.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "toggle [n]vimtree" })
+map.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "focus nvimtr[e]e" })
 
 -------------------------------------- comments ------------------------------------------
 map.set("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current() <CR>", { desc = "toggle comment" })
--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -1,31 +1,11 @@
 return {
 	"windwp/nvim-ts-autotag",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			autotag = {
-				enable = true,
-				enable_rename = true,
-				enable_close = true,
-				enable_close_on_slash = true,
-				filetypes = {
-					"html",
-					"javascript",
-					"typescript",
-					"javascriptreact",
-					"typescriptreact",
-					"svelte",
-					"vue",
-					"tsx",
-					"jsx",
-					"rescript",
-					"xml",
-					"php",
-					"markdown",
-					"astro",
-					"glimmer",
-					"handlebars",
-					"hbs",
-				},
+		require("nvim-ts-autotag").setup({
+			opts = {
+				enable_close = true, -- Auto close tags
+				enable_rename = true, -- Auto rename pairs of tags
+				enable_close_on_slash = true, -- Auto close on trailing </
 			},
 		})
 	end,
